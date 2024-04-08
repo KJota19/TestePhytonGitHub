8460e421d4aa5bd5f0f6a8b170b9e639c55fb07d
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-04-08 17:16:43 -0300

    Create Teste Phyton GitHub2.py

---
+++ Módulos/Teste Phyton GitHub2.py
@@ -0,0 +1,9 @@
+#Cateto oposto e cateto adjacente
+
+import math
+catetoOP=int(input("digite o cateto oposto:"))
+catetoAD=int(input("digite o cateto adjacente:"))
+
+triret=math.hypot(catetoAD,catetoOP)
+
+print(triret)
