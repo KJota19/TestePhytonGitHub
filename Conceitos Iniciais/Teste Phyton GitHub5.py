a6c137db2aed946dcd3eb577d0bdef84cde9824e
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-04-08 17:45:15 -0300

    Create Teste Phyton GitHub5.py

--- Conceitos Iniciais/Teste Phyton GitHub5.py
+++ Conceitos Iniciais/Teste Phyton GitHub5.py
@@ -1,4 +1,5 @@
 #Conversor de R$ para $
+
 dinheiro=float(input("digite quanto de dinheiro você tem: "))
 dolar=dinheiro/4.98
 print("Com R$ {:.2f} você pode comprar U${:.2f}".format(dinheiro,dolar))
