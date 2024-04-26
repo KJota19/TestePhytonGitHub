bc82cb489000e730210e3823cc94af393b16e6fe
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-04-26 14:35:46 -0300

    Create Teste Python GitHub1.py

---
+++ Conceitos Iniciais/Teste Python GitHub1.py
@@ -0,0 +1,12 @@
+#Dados cadastro
+
+nome=input("digite seu nome :")
+altura=float(input("digite sua altura :"))
+idade=int(input("digite sua idade :"))
+
+ano = 2024 - idade
+
+print("o nome é {}".format(nome))
+print("a altura é {}".format(altura))
+print("a idade é {}".format(idade))
+print("o ano de nascimento é {}".format(ano))
