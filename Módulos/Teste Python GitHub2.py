6a530cb7cc3d2672263d42dd34c55b880ac420b4
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-04-26 14:39:30 -0300

    Create Teste Python GitHub2.py

---
+++ Módulos/Teste Python GitHub2.py
@@ -0,0 +1,10 @@
+#Cateto oposto E Cateto Adjacente
+
+import math
+
+catetoOP=int(input("digite o cateto oposto:"))
+catetoAD=int(input("digite o cateto adjacente:"))
+
+triret=math.hypot(catetoAD,catetoOP)
+
+print(triret)
