bb5f43533f2916ef8f5e8016b5983c6440ffdaf6
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-04-26 14:34:57 -0300

    Create Teste Python GitHub4.py

--- Conceitos Iniciais/Teste Phyton GitHub4.py
+++
@@ -1,8 +0,0 @@
-#Média dos alunos
-
-print("MÉDIA")
-aluno=input("nome:")
-nota1=float(input("nota1:"))
-nota2=float(input("nota2:"))
-média=(nota1+nota2)/2
-print("A média do aluno é {}".format(aluno,média))
