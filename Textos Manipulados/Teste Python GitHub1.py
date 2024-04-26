b54d5fbe954a27b9adb9d7f766c50d0de62fb2b2
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-04-26 14:40:36 -0300

    Create Teste Python GitHub1.py

---
+++ Textos Manipulados/Teste Python GitHub1.py
@@ -0,0 +1,15 @@
+#Nome Manipulado
+
+1.
+nome=input("Digite seu nome completo:")
+
+maiusculo=nome.upper()
+print("o nome com todas as letras maiúsculas : {}".format(maiusculo))
+minusculo=nome.lower()
+print("o nome com todas a letras minúsculas: {}".format(minusculo))
+qletrasespa=nome.strip()
+qletrasespa=len(qletrasespa)
+print("a quantidade de letras sem considerar os espaços: {}".format(qletrasespa))
+qtprimeiroN=nome.split()
+qtprimeiroN=len(qtprimeiroN[0])
+print("a quantidade de letras no primeiro nome: {}".format(qtprimeiroN))
