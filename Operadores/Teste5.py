8b2f957aa58a50948fdb0898757bb7f17592fc8a
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 17:03:38 -0300

    Create Teste Python GitHub5.py

---
+++ Operadores/Teste5.py
@@ -0,0 +1,8 @@
+km=int(input("Digite a distância da viagem em KM:"))
+
+if km <= 200:
+    preco1=0.50 * km
+    print("a viagem de {} KM custará R${}".format(km,preco1))
+else:
+    preco2=0.45 * km
+    print("a viagem de {} KM custará R${}".format(km,preco2))
