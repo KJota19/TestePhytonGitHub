b54155528e90f5d00395af9b031b58fe1402aa09
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:58:30 -0300

    Create Teste Python GitHub5.py

---
+++ Conceitos Iniciais/Teste5.py
@@ -0,0 +1,5 @@
+#Conversor De R$ Para $
+
+dinheiro=float(input("digite quanto de dinheiro você tem: "))
+dolar=dinheiro/4.98
+print("Com R$ {:.2f} você pode comprar U${:.2f}".format(dinheiro,dolar))
