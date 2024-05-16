f8a5f8bb3c73b6548f4447b91517b123daea2847
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:58:06 -0300

    Create Teste Python GitHub3.py

---
+++ Conceitos Iniciais/Teste3.py
@@ -0,0 +1,10 @@
+#Area De Um Trapézio
+
+b=int(input("digite a base menor:"))
+B=int(input("digite a base maior:"))
+a=int(input("digite a altura:"))
+
+A=(B+b)/2*a
+area=A**2
+
+print(" A area do trapézio é {}".format(area))
