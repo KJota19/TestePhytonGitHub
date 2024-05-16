63fc1af1d9c8d81042a67695c341f82d9beed1f3
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 17:01:31 -0300

    Create Teste Python GitHub1.py

---
+++ Módulos/Teste1.py
@@ -0,0 +1,10 @@
+#Seno, Cosseno, Tangente
+
+import math
+angulo=float(input("digite um angulo:"))
+
+seno=math.sin(angulo)
+cosseno=math.cos(angulo)
+tangente=math.tan(angulo)
+
+print("seno: {} cosseno: {} e tangente: {}".format(seno,cosseno,tangente))
